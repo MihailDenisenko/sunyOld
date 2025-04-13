25764fb55de3988bc30ec9dfd338e358a60da279
Mihail Denisenko <i@mihail-denisenko.ru>
2025-04-13 22:05:40 +0300

    adding_navigation-13.04.2025-22:00

--- src/content/OurNews/OurNews.jsx
+++ src/content/OurNews/OurNews.jsx
@@ -1,32 +1,36 @@
 import React from "react";
 import styles from "./OurNews.module.scss";
 import { useSelector } from "react-redux";
-import { Carousel } from "nuka-carousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay,  Pagination } from "swiper/modules";
+// import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 
 export default function OurNews() {
   const { ourNews } = useSelector((state) => state.service);
   const { homeUrl } = useSelector((state) => state.urlSlicer);
 
   const news = ourNews.map((n, i) => {
-    const { logo, price, colors, title, isNew} = n;
+    const { logo, price, colors, title, isNew } = n;
     return isNew ? (
       <li key={i} className={styles.card}>
         <div className={styles.card__backImg}>
-        {isNew ? (
+          {isNew ? (
+            <img
+              className={styles.card__isNew}
+              src="https://avatars.mds.yandex.net/i?id=4cd65be8dc5bcc969a0190d2b9cc3cd1b6c4bd25bc196711-12714867-images-thumbs&n=13"
+            />
+          ) : (
+            ""
+          )}
+          <img src={`${homeUrl}${logo}`} className={styles.card__logo} alt="" />
           <img
-            className={styles.card__isNew}
-            src="https://avatars.mds.yandex.net/i?id=4cd65be8dc5bcc969a0190d2b9cc3cd1b6c4bd25bc196711-12714867-images-thumbs&n=13"
+            src={`${homeUrl}${colors}`}
+            className={styles.card__colors}
+            alt=""
           />
-        ) : (
-          ""
-        )}
-        <img src={`${homeUrl}${logo}`} className={styles.card__logo} alt="" />
-        <img
-          src={`${homeUrl}${colors}`}
-          className={styles.card__colors}
-          alt=""
-          />
-          </div>
+        </div>
         <p className={styles.card__title}>{title}</p>
         <p className={styles.card__price}>{`от ${price} ₽`}</p>
         <div className={styles.card__button}>Подробнее</div>
@@ -39,7 +43,59 @@ export default function OurNews() {
   return (
     <div className={styles.ourNews} style={{ textAlign: "center" }}>
       <span className={styles.ourNews__span}>Наши новинки</span>
-      <ul className={styles.ul}>{ news}</ul>
+      {/* <ul className={styles.ul}>{news}</ul> */}
+      <Swiper
+        breakpoints={{
+          320: { slidesPerView: 1 },
+          640: { slidesPerView: 2 },
+          768: { slidesPerView: 3 },
+          1024: { slidesPerView: 5 },
+        }}
+        loop={true}
+        spaceBetween={30}
+        className={styles.ul}
+        autoplay={{
+          delay: 500,
+          disableOnInteraction: false,
+        }}
+        slidesPerView={1}
+        effect="fade"
+        fadeEffect={{ crossFade: true }}
+        pagination={{ clickable: true }}
+      >
+        {ourNews.map((n, i) => {
+          const { logo, price, colors, title, isNew } = n;
+
+          return (
+            <SwiperSlide key={i} className={styles.card}>
+              <div className={styles.card__backImg}>
+                {isNew ? (
+                  <img
+                    className={styles.card__isNew}
+                    src="https://avatars.mds.yandex.net/i?id=4cd65be8dc5bcc969a0190d2b9cc3cd1b6c4bd25bc196711-12714867-images-thumbs&n=13"
+                  />
+                ) : (
+                  ""
+                )}
+                <img
+                  src={`${homeUrl}${logo}`}
+                  className={styles.card__logo}
+                  alt=""
+                />
+                <img
+                  src={`${homeUrl}${colors}`}
+                  className={styles.card__colors}
+                  alt=""
+                />
+              </div>
+              <p className={styles.card__title}>{title}</p>
+              <p className={styles.card__price}>{`от ${price} ₽`}</p>
+              <div className={styles.card__button}>Подробнее</div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <div className={styles.bottom}></div>
     </div>
   );
 }
