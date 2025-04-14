d4454895ae89dc58d1f36a0203749df021eecc28
Mihail Denisenko <i@mihail-denisenko.ru>
2025-04-14 20:04:44 +0300

    adding_navigation-13.04.2025-22:00

--- src/content/OurNews/OurNews.jsx
+++ src/content/OurNews/OurNews.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import styles from "./OurNews.module.scss";
-import useEmblaCarousel from "embla-carousel-react";
-import Autoplay from "embla-carousel-autoplay";
+
 
 export default function OurNews() {
   const { ourNews } = useSelector((state) => state.service);
   const { homeUrl } = useSelector((state) => state.urlSlicer);
-  const [emblaRef] = useEmblaCarousel({loop:true},[Autoplay()]);
+
+   const params = {
+     spaceBetween: 30,
+     centeredSlides: true,
+     autoplay: {
+       delay: 2500,
+       disableOnInteraction: false,
+     },
+     pagination: {
+       el: ".swiper-pagination",
+       clickable: true,
+     },
+     navigation: {
+       nextEl: ".swiper-button-next",
+       prevEl: ".swiper-button-prev",
+     },
+   };
+
+
 
   const news = ourNews.map((n, i) => {
     const { logo, price, colors, title, isNew } = n;
@@ -37,50 +54,15 @@ export default function OurNews() {
       ""
     );
   });
-  console.log(ourNews)
+  console.log(ourNews);
 
   return (
     <div className={styles.ourNews} style={{ textAlign: "center" }}>
       <span className={styles.ourNews__span}>Наши новинки</span>
       {/* <ul className={styles.ul}>{news}</ul> */}
 
-      <div className={`${styles.embla}`} ref={emblaRef}>
-        <div className={styles.ul}>
-            {ourNews.map((n, i) => {
-              const { logo, price, colors, title, isNew } = n;
-              return isNew ? (
-                <li key={i} className={styles.card}>
-                  <div className={styles.card__backImg}>
-                    {isNew ? (
-                      <img
-                        className={styles.card__isNew}
-                        src="https://avatars.mds.yandex.net/i?id=4cd65be8dc5bcc969a0190d2b9cc3cd1b6c4bd25bc196711-12714867-images-thumbs&n=13"
-                      />
-                    ) : (
-                      ""
-                    )}
-                    <img
-                      src={`${homeUrl}${logo}`}
-                      className={styles.card__logo}
-                      alt=""
-                    />
-                    <img
-                      src={`${homeUrl}${colors}`}
-                      className={styles.card__colors}
-                      alt=""
-                    />
-                  </div>
-                  <p className={styles.card__title}>{title}</p>
-                  <p className={styles.card__price}>{`от ${price} ₽`}</p>
-                  <div className={styles.card__button}>Подробнее</div>
-                </li>
-              ) : (
-                ""
-              );
-            })}
+      
 
-        </div>
-      </div>
 
       <div className={styles.bottom}></div>
     </div>
